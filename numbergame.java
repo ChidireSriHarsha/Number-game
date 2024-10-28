36f243970680abea48d25fe2c6d54762808a1d52
ChidireSriHarsha <chideresriharsha@gmail.com>
2024-10-28 19:52:37 +0530

    Add files via upload
    Number-game
    Student Grade Calculator
    Student Course Registration System

---
+++ numbergame.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+class RangeGenerator {
+    public int generate(int max, int min) {
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
+}
+
+public class NumberGame {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        RangeGenerator rg = new RangeGenerator();
+        int totalAttempts = 0;
+        int win = 0;
+
+        while (true) {
+            System.out.println("Enter the maximum number:");
+            int max = sc.nextInt();
+            System.out.println("Enter the minimum number:");
+            int min = sc.nextInt();
+            sc.nextLine();
+
+            int cnum = rg.generate(max, min);
+            int attempts = 0;
+
+            while (true) {
+                System.out.println("Guess a number between " + min + " and " + max + ":");
+                int gnum = sc.nextInt();
+                attempts++;
+
+                if (gnum > cnum) {
+                    System.out.println("It's Greater");
+                } else if (gnum < cnum) {
+                    System.out.println("It's Lower");
+                } else {
+                    System.out.println("Correct Guess!");
+                    win++;
+                    break;
+                }
+            }
+            totalAttempts += attempts;
+            System.out.println("Attempts = " + attempts);
+            System.out.println("Wins = " + win);
+
+            double winRate = (double) win / totalAttempts * 100;
+            System.out.printf("Your win rate is %.2f%%\n", winRate);
+
+            System.out.println("Do you want to play again? (yes/no)");
+            String playAgain = sc.next();
+            if (!playAgain.equalsIgnoreCase("yes")) {
+                sc.close();
+                System.exit(0);
+            }
+            sc.nextLine();
+        }
+    }
+}
